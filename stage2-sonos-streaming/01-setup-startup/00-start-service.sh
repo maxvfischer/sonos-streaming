2e29a72e9f94e37300154ce4d97e8786ba9cb494
Max Fischer <max.fischer@xorlab.com>
2023-04-29 08:33:10 +0200

    WIP: Try to fix problem with files

--- stage2-sonos-streaming/01-setup-startup/00-start-service.sh
+++ stage2-sonos-streaming/01-setup-startup/00-start-service.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
-HOME="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
-
 # Copy systemctl service file
-install -m 755 -o 1000 -g 1000 files/sonos-streaming.service /etc/systemd/system/
+install -v -m 755 -o 1000 -g 1000 files/sonos-streaming.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 # Enable the sonos streaming service
 on_chroot << EOF
