7413a5dddcb3beacfc627c417e7bdc73f53998d1
Max Fischer <max.v.fischer@gmail.com>
2023-05-25 16:10:59 +0100

    Add docker installation script

---
+++ install-docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Docker installation (from https://docs.docker.com/engine/install/debian/)
+## Set up the repository
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+## Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
