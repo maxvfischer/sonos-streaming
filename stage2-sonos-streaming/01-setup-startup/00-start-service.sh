a42892d5ebae37ab3d1588f57708ba84eec4cac0
Max Fischer <max.fischer@xorlab.com>
2023-05-25 17:15:58 +0200

    Remove files

--- stage2-sonos-streaming/01-setup-startup/00-start-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Copy systemctl service file
-install -v -m 755 -o 1000 -g 1000 files/sonos-streaming.service "${ROOTFS_DIR}/etc/systemd/system/"
-
-# Enable the sonos streaming service
-on_chroot << EOF
-  systemctl enable sonos-streaming.service
-EOF
